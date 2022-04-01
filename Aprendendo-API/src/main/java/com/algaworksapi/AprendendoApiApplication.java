5a11f126269965dfd40cb3a49bee29579f122e27
Alisson-Jns <alisson.junio@hotmail.com>
2022-03-31 22:17:03 -0300

    Criação do projeto

---
+++ Aprendendo-API/src/main/java/com/algaworksapi/AprendendoApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApiApplication.class, args);
+	}
+
+}
---
+++ Aprendendo-API/src/test/java/com/algaworksapi/AprendendoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
